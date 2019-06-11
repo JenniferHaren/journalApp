33311002ec0c0ce24236f7cc455e6dfd8d098d0a
Jennifer Haren <jennifer.haren110@gmail.com>
2019-06-11 12:00:20 -0700

    Add basic home page for app, include templates
    Used header and footer templates to create a home page for the app.  Page includes links to a Create page, where new users will be created, and a Read page, where users can read all of their previous journal entries.

---
+++ public/index.php
@@ -0,0 +1,15 @@
+ <?php include "templates/header.php"; ?>
+
+ <ul>
+
+  <li>
+    <a href="create.php"><strong>Create</strong></a> - New User
+  </li>
+
+  <li>
+    <a href="read.php"><strong>Read</strong></a> - Find My Entries
+  </li>
+
+</ul>
+
+<?php include "templates/footer.php"; ?>
